d81bc58569a0ad496d00fe51a1d265e38a4e6be7
Kelly Keene <kellykeene@Kellys-MacBook-Pro.local>
2024-01-24 14:41:47 -0800

    Added additional Bubble sort implementations, fixed tsconfig to emit js files to dist folder.

--- src/BubbleSort.ts
+++ src/BubbleSort.ts
@@ -1,5 +1,5 @@
 // O(n^2)
-export default function bubbleSort(arr: number[]): void {
+export function bubbleSortBruteForce(arr: number[]): void {
     // [ 3, 420, 4, 69, 7, 9, 42 ]
     //  i , j   a[i]  a[j]  a[j+1]    arr.length  (arr.length - 1 - i)   array
     // -------|------|-----|--------|------------|---------------------|------------------------------
@@ -19,8 +19,6 @@ export default function bubbleSort(arr: number[]): void {
     //  2, 3  |  7   |  9  |   42   |     7      |   7 - 1 - 2 = 4     | [ 3, 4, 7, 9, 42, 69, 420 ] SORTED!
 
     // O(n^2)
-    //
-
     for (let i = 0; i < arr.length; ++i) {
         for (let j = 0; j < arr.length - 1 - i; ++j) {
             if (arr[j] > arr[j + 1]) {
@@ -30,4 +28,71 @@ export default function bubbleSort(arr: number[]): void {
             }
         }
     }
+}
+
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+export function bubbleSortUnoptimized(arr: number[]) {
+    for (var i = arr.length; i > 0; i--) {
+        for (var j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// ES2015 Version
+export function bubbleSortES2015(arr: number[]) {
+    const swap = (arr: number[], idx1: number, idx2: number) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    return arr;
+}
+
+// Optimized BubbleSort with noSwaps
+export function bubbleSortNoSwaps(arr: number[]) {
+    var noSwaps;
+    for (var i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for (var j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) break;
+    }
+    return arr;
+}
+
+export function bubbleSortAnotherWay(arr: number[]) {
+    const swap = (arr: number[], i: number, j: number) => {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    };
+
+    // [ 3, 420, 4, 69, 7, 9, 42 ]
+    for (let i = arr.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    
+    return arr;
 }
--- tests/BubbleSort.test.ts
+++ tests/BubbleSort.test.ts
@@ -1,7 +1,41 @@
-import bubbleSort from "../src/BubbleSort";
+import {
+    bubbleSortBruteForce,
+    bubbleSortUnoptimized,
+    bubbleSortES2015,
+    bubbleSortNoSwaps,
+    bubbleSortAnotherWay,
+} from "../src/BubbleSort";
 
-test("Bubble Sort", function () {
+test("bubbleSortBruteForce", function () {
     const arr = [9, 3, 7, 4, 69, 420, 42];
-    bubbleSort(arr);
+    bubbleSortBruteForce(arr);
     expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+});
+
+test("bubbleSortUnoptimized", function () {
+    const arr = [9, 3, 7, 4, 69, 420, 42];
+    bubbleSortUnoptimized(arr);
+    expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+});
+
+test("bubbleSortES2015", function () {
+    const arr = [9, 3, 7, 4, 69, 420, 42];
+    bubbleSortES2015(arr);
+    expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+});
+
+test("bubbleSortNoSwaps", function () {
+    const arr = [9, 3, 7, 4, 69, 420, 42];
+    bubbleSortNoSwaps(arr);
+    expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+});
+
+test("bubbleSortAnotherWay", function () {
+    const arr = [
+        2183, 17358, 46676, 68027, 90467, 101257, 119637, 128114, 149770,
+    ];
+    bubbleSortAnotherWay(arr);
+    expect(arr).toEqual([
+        2183, 17358, 46676, 68027, 90467, 101257, 119637, 128114, 149770,
+    ]);
 });
